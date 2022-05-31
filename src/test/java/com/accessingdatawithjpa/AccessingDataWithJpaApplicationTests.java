6335c3976c22709da58ee8beb6c59ae46d7ca05c
ndcalabrese <ndcalabrese@gmail.com>
2022-05-31 14:52:58 -0400

    Initial commit

---
+++ src/test/java/com/accessingdatawithjpa/AccessingDataWithJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.accessingdatawithjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccessingDataWithJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
